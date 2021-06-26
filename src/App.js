b02217b6e44fa94eaf22cd37e7f82a57c4a06ca7
MrDebugCode <hilarior257@gmail.com>
2021-06-25 23:40:54 -0600

    Se agrego la carga condicional de los componentes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   // Definir el state
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarPregunta, actualizarPregunta ] = useState(true);
 
 
   return (
@@ -16,19 +17,21 @@ function App() {
             <h1>Administra tu presupuesto</h1>
 
             <div className="contenido-principal contenido">
+              {mostrarPregunta ? (
                 <Pregunta
-                  guardarPresupuesto={guardarPresupuesto}
-                  guardarRestante={guardarRestante}
-                />
-
-                <div className="row">
-                  <div className="one-half column">
-                    <Formulario />
+                    guardarPresupuesto={guardarPresupuesto}
+                    guardarRestante={guardarRestante}
+                    actualizarPregunta={actualizarPregunta}
+                  />) : (
+                  <div className="row">
+                    <div className="one-half column">
+                      <Formulario />
+                    </div>
+                    <div className="one-half column">
+                      2
+                    </div>
                   </div>
-                  <div className="one-half column">
-                    2
-                  </div>
-                </div>
+                  )}
             </div>
 
         </header>
