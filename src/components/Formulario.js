8dbc58e3ce747e96831612555063d03c33dcfc02
MrDebugCode <hilarior257@gmail.com>
2021-06-26 14:06:30 -0600

    Validando los formularios de agregar gastos, se añadio la libreria 'shortid'

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,32 @@
 import React, {useState} from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+
 const Formulario = () => {
 
     const [nombre, guardarNombre] = useState('');
     const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
 
     const agregarGasto = e => {
         e.preventDefault();
+
         //Validar
-        //construir el gasto
+        if(cantidad < 1 || isNaN(cantidad) || nombre.trim()  === '' ){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+       //construir el gasto
+
+       const gasto = {
+           nombre,
+           cantidad,
+           id: shortid.generate()
+       }
+
+       console.log(gasto);
         //pasar el gasto al componente principal
         //resetear el form
     }
@@ -17,6 +36,7 @@ const Formulario = () => {
             onSubmit={agregarGasto}
         >
             <h2>Agrega tu gastos aquí</h2>
+            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" /> : null}
 
             <div className="campo">
                 <label>Nombre del Gasto</label>
